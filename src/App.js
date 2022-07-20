b9097d145f66347946d5cd6264c125ed59b60098
songexile <therunningbeta@gmail.com>
2022-07-20 18:38:48 +1200

    fix

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import MainPage from './pages/MainPage';
 import Footer from './components/Footer';
 import Projects from './pages/Projects';
 
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
+    
     <div className="App">
       <Navbar/>
       <MainPage/>
